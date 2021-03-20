bd384e27a22f1b605b085aa4153449ec2817d53b
Rogue Alexander <rotkosky.rogue@gmail.com>
2021-03-20 23:58:45 +0100

    feature(General): Create grid page wrapper component for use in wallet and pool

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -5,12 +5,12 @@ export const GridPageWrapper = styled.div<{
   cardWidth?: string
   justify?: 'stretch' | 'center' | 'start' | 'end' | 'flex-start' | 'flex-end' | 'space-between'
 }>`
-  padding-top: 150px;
+  padding-bottom: 60px;
   max-width: 1200px;
   width: 100%;
   display: grid;
   grid-auto-rows: auto;
-  grid-row-gap: ${({ gap }) => (gap === 'sm' && '8px') || (gap === 'md' && '12px') || (gap === 'lg' && '24px') || gap};
+  grid-gap: ${({ gap }) => (gap === 'sm' && '8px') || (gap === 'md' && '12px') || (gap === 'lg' && '24px') || gap};
   grid-template-columns: ${({ cardWidth }) => `repeat(auto-fit, minmax(${cardWidth ?? 350}px, 1fr))`};
   justify-items: ${({ justify }) => justify && justify};
 `

51d1eb877d267ce03d060a5f52ca28fbada0d5ac
Rogue Alexander <rotkosky.rogue@gmail.com>
2021-03-22 07:43:51 +0100

    fix(DarkMode): Disable dark mode while it is being worked on

--- src/state/user/hooks.tsx
+++ src/state/user/hooks.tsx
@@ -2,7 +2,7 @@ import { ChainId, Pair, Token } from '@uniswap/sdk'
 import flatMap from 'lodash.flatmap'
 import ReactGA from 'react-ga'
 import { useCallback, useMemo } from 'react'
-import { shallowEqual, useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { BASES_TO_TRACK_LIQUIDITY_FOR, PINNED_PAIRS } from '../../constants'
 
 import { useActiveWeb3React } from '../../hooks'
@@ -43,18 +43,19 @@ function deserializeToken(serializedToken: SerializedToken): Token {
 }
 
 export function useIsDarkMode(): boolean {
-  const { userDarkMode, matchesDarkMode } = useSelector<
-    AppState,
-    { userDarkMode: boolean | null; matchesDarkMode: boolean }
-  >(
-    ({ user: { matchesDarkMode, userDarkMode } }) => ({
-      userDarkMode,
-      matchesDarkMode
-    }),
-    shallowEqual
-  )
+  return true
+  // const { userDarkMode, matchesDarkMode } = useSelector<
+  //   AppState,
+  //   { userDarkMode: boolean | null; matchesDarkMode: boolean }
+  // >(
+  //   ({ user: { matchesDarkMode, userDarkMode } }) => ({
+  //     userDarkMode,
+  //     matchesDarkMode
+  //   }),
+  //   shallowEqual
+  // )
 
-  return userDarkMode === null ? matchesDarkMode : userDarkMode
+  // return userDarkMode === null ? matchesDarkMode : userDarkMode
 }
 
 export function useDarkModeManager(): [boolean, () => void] {
